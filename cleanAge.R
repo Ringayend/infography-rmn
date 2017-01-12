fa092e747a2160d5a0b2dde02027e5f19c25fbee
Laure MARCHAL <laure.marchal@igtp3-mac10.isim.intra>
2017-01-12 14:48:15 +0100

    tableau de contingence des CSP / Accident

--- cleanAge.R
+++ cleanAge.R
@@ -13,3 +13,31 @@ cleanAgeFait<-function(array,index){
   }
   return(array)
 }
+
+cleanCSPFait<-function(array,index){
+  n<-nrow(array)
+  for(i in 1:n){
+    if(array[i,index] ==  "Etudiants" | array[i,index] ==  "En recherche d'emploi"){
+      array[i,index]<-"sans emplois"
+    }
+    else if(array[i,index] ==  "Employés"){
+      array[i,index]<-"employes"
+    }
+    else if(array[i,index] ==  "Cadres"){
+      array[i,index]<-"cadres"
+    }
+    else if(array[i,index] ==  "Ouvriers"){
+      array[i,index]<-"ouvriers"
+    }
+    else if(array[i,index] ==  "Retraités"){
+      array[i,index]<-"retraites"
+    }
+    else if(array[i,index] ==  "Chefs d'entrepirses de 10 salariés de plus"){
+      array[i,index]<-"patrons"
+    }
+    else{
+      array[i,index]<-"autres"
+    }
+  }
+  return(array)
+}
